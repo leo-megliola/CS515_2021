dda7c66ef621a503a0a2ea8862d5714a6e3bd022
leo-megliola <leo.megliola@gmail.com>
2023-02-28 18:56:25 -0800

    Delete 9P_local directory

--- 9P_local/cmake-build-debug/CMakeFiles/9P_local.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for 9P_local.
-# This may be replaced when dependencies are built.
--- 9P_local/maptest.cpp
+++
@@ -1,5 +0,0 @@
-#include "map.h"
-
-int main(){
-    Map<int, int> m; // Map template will be instanticated here
-}
